7a308d8fad343f5aed08584ec680a940163237e7
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-09-13 18:18:38 +0900

    impl: merkle tree

--- src/merkle_tree.rs
+++ src/merkle_tree.rs
@@ -2,10 +2,50 @@ use ark_bn254::Fr;
 use ark_crypto_primitives::crh::TwoToOneCRHScheme as _;
 use ark_crypto_primitives::crh::poseidon::TwoToOneCRH;
 use ark_crypto_primitives::sponge::poseidon::PoseidonConfig;
+use ark_ff::AdditiveGroup;
 use std::collections::HashMap;
 
 pub struct MerkleProof {
     pub siblings: Vec<Fr>,
+}
+
+impl MerkleProof {
+    pub fn dummy(height: usize) -> Self {
+        Self {
+            siblings: vec![Fr::default(); height],
+        }
+    }
+
+    pub fn height(&self) -> usize {
+        self.siblings.len()
+    }
+
+    pub fn get_root(&self, param: &PoseidonConfig<Fr>, leaf_hash: Fr, index: usize) -> Fr {
+        let index_bits = usize_le_bits(index, self.height());
+        let mut state = leaf_hash;
+        for (&bit, sibling) in index_bits.iter().zip(self.siblings.iter()) {
+            state = if bit {
+                two_to_one(&param, *sibling, state)
+            } else {
+                two_to_one(&param, state, *sibling)
+            }
+        }
+        state
+    }
+
+    pub fn verify(
+        &self,
+        param: &PoseidonConfig<Fr>,
+        leaf_hash: Fr,
+        index: usize,
+        merkle_root: Fr,
+    ) -> anyhow::Result<()> {
+        anyhow::ensure!(
+            self.get_root(param, leaf_hash, index) == merkle_root,
+            "Merkle proof verification failed"
+        );
+        Ok(())
+    }
 }
 
 pub fn two_to_one(param: &PoseidonConfig<Fr>, left: Fr, right: Fr) -> Fr {
@@ -21,10 +61,10 @@ pub struct MerkleTree {
 }
 
 impl MerkleTree {
-    pub fn new(poseidon_params: PoseidonConfig<Fr>, height: usize, empty_leaf_hash: Fr) -> Self {
+    pub fn new(poseidon_params: PoseidonConfig<Fr>, height: usize) -> Self {
         // zero_hashes = reverse([H(zero_leaf), H(H(zero_leaf), H(zero_leaf)), ...])
         let mut zero_hashes = vec![];
-        let mut h = empty_leaf_hash;
+        let mut h = Fr::ZERO;
         zero_hashes.push(h.clone());
         for _ in 0..height {
             h = two_to_one(&poseidon_params, h, h);
@@ -106,4 +146,34 @@ fn usize_le_bits(num: usize, length: usize) -> Vec<bool> {
         n >>= 1;
     }
     result
+}
+
+#[cfg(test)]
+mod tests {
+    use alloy::signers::k256::elliptic_curve::rand_core::OsRng;
+    use ark_bn254::Fr;
+    use ark_ff::UniformRand as _;
+    use folding_schemes::transcript::poseidon::poseidon_canonical_config;
+
+    use crate::merkle_tree::MerkleTree;
+
+    #[test]
+    fn test_merkle_tree() {
+        let poseidon_params = poseidon_canonical_config::<Fr>();
+        let height = 3;
+
+        let mut tree = MerkleTree::new(poseidon_params.clone(), height);
+
+        let leaf_hashes: Vec<Fr> = (0..(1 << height)).map(|_| Fr::rand(&mut OsRng)).collect();
+
+        for (i, leaf_hash) in leaf_hashes.iter().enumerate() {
+            tree.update_leaf(i, *leaf_hash);
+            let proof = tree.prove(i);
+
+            // verify proof
+            proof
+                .verify(&poseidon_params, *leaf_hash, i, tree.get_root())
+                .unwrap();
+        }
+    }
 }
