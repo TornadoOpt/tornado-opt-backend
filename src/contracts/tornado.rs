5c2ed4b2a55a0f7e401fd61f3b46e8c7e74fe27c
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-09-13 18:51:57 +0900

    feat(wip): fetch deposit event

--- src/contracts/tornado.rs
+++ src/contracts/tornado.rs
@@ -11,6 +11,12 @@ sol!(
 );
 
 #[derive(Debug, Clone)]
+pub struct DepositEvent {
+    pub index: u64,
+    pub commitment: B256,
+}
+
+#[derive(Debug, Clone)]
 pub struct TornadoContract {
     pub provider: NormalProvider,
     pub address: Address,
@@ -21,37 +27,57 @@ impl TornadoContract {
         Self { provider, address }
     }
 
+    pub async fn get_deposit_events(
+        &self,
+        from_eth_block: u64,
+        to_eth_block_number: u64,
+    ) -> Result<Vec<DepositEvent>, BlockchainError> {
+        let contract = Tornado::new(self.address, self.provider.clone());
+        let events = contract
+            .event_filter::<Tornado::Deposit>()
+            .address(self.address)
+            .from_block(from_eth_block)
+            .to_block(to_eth_block_number)
+            .query()
+            .await?;
+        let mut deposit_events = Vec::new();
+        for (event, meta) in events {
+            deposit_events.push(DepositEvent {
+                index: event.depositIndex,
+                commitment: event.commitment,
+            });
+        }
+        deposit_events.sort_by_key(|event| event.index);
+        Ok(deposit_events)
+    }
+
     pub async fn get_hash_chain_root(&self) -> Result<B256, BlockchainError> {
         let tornado = Tornado::new(self.address, self.provider.clone());
         let root = tornado.hashChainRoot().call().await?;
         Ok(root)
     }
 
-    // pub async fn get_deposit_data(&self, deposit_id: u64) -> Result<DepositData, BlockchainError> {
-    //     let int1 = Int1::new(self.address, self.provider.clone());
-    //     let data = int1.getDepositData(U256::from(deposit_id)).call().await?;
-    //     let data = DepositData {
-    //         deposit_hash: Bytes32::from_bytes_be(data.depositHash.as_ref()),
-    //         sender: data.sender,
-    //         is_rejected: data.isRejected,
-    //     };
-    //     Ok(data)
-    // }
-
-    // pub async fn get_withdrawal_nullifier_exists(
+    // pub async fn deposit_native_token(
     //     &self,
-    //     nullifier: Bytes32,
-    // ) -> Result<bool, BlockchainError> {
-    //     let int1 = Int1::new(self.address, self.provider.clone());
-    //     let nullifier = convert_bytes32_to_b256(nullifier);
-    //     let block_number = int1.nullifiers(nullifier).call().await?;
-    //     Ok(!block_number.is_zero())
-    // }
-
-    // pub async fn get_last_processed_deposit_id(&self) -> Result<u64, BlockchainError> {
-    //     let int1 = Int1::new(self.address, self.provider.clone());
-    //     let id = int1.getLastProcessedDepositId().call().await?;
-    //     Ok(id.to())
+    //     signer_private_key: B256,
+    //     recipient_salt_hash: Bytes32,
+    //     value: U256,
+    // ) -> Result<TxHash, BlockchainError> {
+    //     let signer = get_provider_with_signer(&self.provider, signer_private_key);
+    //     let contract = Int1::new(self.address, signer.clone());
+    //     let tx_request = contract
+    //         .depositNativeToken(convert_bytes32_to_b256(recipient_salt_hash))
+    //         .value(value)
+    //         .into_transaction_request();
+    //     let tx_hash = send_transaction_with_gas_bump(
+    //         &self.provider,
+    //         signer,
+    //         tx_request,
+    //         "deposit_native_token",
+    //         "depositor",
+    //     )
+    //     .await?;
+    //     Ok(tx_hash)
     // }
 
     // pub async fn withdrawal(
@@ -78,58 +104,6 @@ impl TornadoContract {
     //         tx_request,
     //         "withdrawal",
     //         "withdrawer",
-    //     )
-    //     .await?;
-    //     Ok(tx_hash)
-    // }
-
-    // pub async fn cancel_deposit(
-    //     &self,
-    //     signer_private_key: B256,
-    //     deposit_id: u64,
-    //     recipient_salt_hash: Bytes32,
-    //     token_index: u32,
-    //     amount: U256,
-    // ) -> Result<TxHash, BlockchainError> {
-    //     let signer = get_provider_with_signer(&self.provider, signer_private_key);
-    //     let contract = Int1::new(self.address, signer.clone());
-    //     let deposit = Deposit {
-    //         recipientSaltHash: convert_bytes32_to_b256(recipient_salt_hash),
-    //         tokenIndex: token_index,
-    //         amount,
-    //     };
-    //     let tx_request = contract
-    //         .cancelDeposit(U256::from(deposit_id), deposit)
-    //         .into_transaction_request();
-    //     let tx_hash = send_transaction_with_gas_bump(
-    //         &self.provider,
-    //         signer,
-    //         tx_request,
-    //         "cancel_deposit",
-    //         "depositor",
-    //     )
-    //     .await?;
-    //     Ok(tx_hash)
-    // }
-
-    // pub async fn deposit_native_token(
-    //     &self,
-    //     signer_private_key: B256,
-    //     recipient_salt_hash: Bytes32,
-    //     value: U256,
-    // ) -> Result<TxHash, BlockchainError> {
-    //     let signer = get_provider_with_signer(&self.provider, signer_private_key);
-    //     let contract = Int1::new(self.address, signer.clone());
-    //     let tx_request = contract
-    //         .depositNativeToken(convert_bytes32_to_b256(recipient_salt_hash))
-    //         .value(value)
-    //         .into_transaction_request();
-    //     let tx_hash = send_transaction_with_gas_bump(
-    //         &self.provider,
-    //         signer,
-    //         tx_request,
-    //         "deposit_native_token",
-    //         "depositor",
     //     )
     //     .await?;
     //     Ok(tx_hash)
