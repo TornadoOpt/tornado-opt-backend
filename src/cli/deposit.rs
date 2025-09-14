f91b1168bec15ddc540e7b79320e1b8e8a75fe38
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-09-14 18:32:42 +0900

    feat: made placeholder

--- src/cli/deposit.rs
+++ src/cli/deposit.rs
@@ -2,6 +2,6 @@ use alloy::primitives::B256;
 
 use crate::state::State;
 
-pub async fn deposit_operation(state: &mut State, private_key: B256) -> anyhow::Result<()> {
+pub async fn deposit_operation(_state: &mut State, _private_key: B256) -> anyhow::Result<()> {
     Ok(())
 }
--- src/cli/withdrawal.rs
+++ src/cli/withdrawal.rs
@@ -3,10 +3,10 @@ use alloy::primitives::B256;
 use crate::state::State;
 
 pub async fn withdrawal_operation(
-    state: &mut State,
-    nullifier: B256,
-    secret: B256,
-    private_key: B256,
+    _state: &mut State,
+    _nullifier: B256,
+    _secret: B256,
+    _private_key: B256,
 ) -> anyhow::Result<()> {
     Ok(())
 }
